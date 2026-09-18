8f3c5926fbd2749b751509f57fa7df7b0db8e02c
Ayushgiggit <ayushmaurya9450@gmail.com>
2026-09-18 22:16:34 +0530

    Random Number

--- selenium-java/src/main/java/genric_utility/JavaUtility.java
+++ selenium-java/src/main/java/genric_utility/JavaUtility.java
@@ -4,15 +4,8 @@ import java.util.Random;
 
 public class JavaUtility  {
 
-		public static  int generateRandomNumber(int limit) {		
+		public  int generateRandomNumber(int limit) {		
 					Random zc = new Random();
 					return zc.nextInt(limit);
 				}
-
-public static void main(String[] args) {
-
-	int num = generateRandomNumber(1000);
-
-	System.out.println(num);
-}
 }
